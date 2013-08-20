f9dfca244bda6edf0dccced7ff9d3cd9bc196764
Jan Guegel <j.guegel@gmail.com>
2013-08-20 18:15:36 +0200

    string

--- Bierdienung/src/de/ur/mi/ausschank_kueche/Ausschank.java
+++ Bierdienung/src/de/ur/mi/ausschank_kueche/Ausschank.java
@@ -8,7 +8,7 @@ import android.view.Menu;
 public class Ausschank extends Activity{
 	
 
-	
+	private String string;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
