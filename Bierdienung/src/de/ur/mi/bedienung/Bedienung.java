d39a93991f20183f9044af42fcb73c77e9e65394
JonE <JonE@192.168.1.9>
2013-08-21 09:46:03 +0200

    Bedienung -> Getränke // tischnummer eingeben und es wird entweder neue tabelle angelegt oder alte aufgerufen zum bearbeiten funktioniert schon mal...

--- Bierdienung/src/de/ur/mi/bedienung/Bedienung.java
+++ Bierdienung/src/de/ur/mi/bedienung/Bedienung.java
@@ -1,18 +1,47 @@
 package de.ur.mi.bedienung;
 
 import de.ur.bierdienung.R;
+
+import de.ur.mi.parse.ToDoListActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Bedienung extends Activity{
 	
-	private String string = "es arschlecher";
+	private EditText tischNummer;
+	private Button getraenkeButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bedienung);
-
+		getraenkeButton = (Button) findViewById(R.id.getraenke);
+		tischNummer = (EditText) findViewById(R.id.nr);
+		getraenkeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Intent i = new Intent(Bedienung.this,
+						ToDoListActivity.class);
+				i.putExtra("tisch", Integer.parseInt(tischNummer.getText().toString()));
+				startActivity(i);
+				
+				
+				
+				
+				
+				
+				
+			}
+		});
+		
 	}
 
 	@Override
