6777b41832686b2dedc55562722f006c59f00ac7
Max Feigl <max-feigl@web.de>
2013-09-14 13:20:30 +0200

    test menü mit tischAbrechnung

--- Bierdienung/src/de/ur/mi/bierdienung/SpeiseKartenActivity.java
+++ Bierdienung/src/de/ur/mi/bierdienung/SpeiseKartenActivity.java
@@ -25,135 +25,140 @@ import de.ur.mi.parse.ParselistdownloadClass;
 
 public class SpeiseKartenActivity extends Activity {
 
-	// Declare Variables
-	ListView listview;
-	List<ParseObject> ob;
-	ProgressDialog mProgressDialog;
-	ListViewAdapter adapter;
-	private List<ParselistdownloadClass> parselistdownloadList = null;
+    // Declare Variables
+    ListView listview;
+    List<ParseObject> ob;
+    ProgressDialog mProgressDialog;
+    ListViewAdapter adapter;
+    private List<ParselistdownloadClass> parselistdownloadList = null;
 
-	public static final int INSERT_ID = Menu.FIRST;
-	public static final int TISCH_WECHSELN_ID = Menu.FIRST + 1;
+    public static final int INSERT_ID = Menu.FIRST;
+    public static final int TISCH_WECHSELN_ID = Menu.FIRST + 1;
 
-	private String karte;
+    private String karte;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Get the view from listview_main.xml
-		setContentView(R.layout.activity_speisekarte);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the view from listview_main.xml
+        setContentView(R.layout.activity_speisekarte);
 
-		Bundle extras = getIntent().getExtras();
-		karte = extras.getString("name");
+        Bundle extras = getIntent().getExtras();
+        karte = extras.getString("name");
 
-		setTitle("Tisch " + BedienungTischAuswahlActivity.getTNR());
+        setTitle("Tisch " + BedienungTischAuswahlActivity.getTNR());
 
-		// Execute RemoteDataTask AsyncTask
-		new RemoteDataTask().execute();
+        // Execute RemoteDataTask AsyncTask
+        new RemoteDataTask().execute();
 
-	}
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
 
-		case R.id.tisch:
-			Intent iTisch = new Intent(SpeiseKartenActivity.this,
-					TischActivity.class);
-			startActivity(iTisch);
-			finish();
-			return true;
+            case R.id.tisch:
+                Intent iTisch = new Intent(SpeiseKartenActivity.this,
+                        TischActivity.class);
+                startActivity(iTisch);
+                finish();
+                return true;
 
-		case R.id.speisekarte:
-			Intent iEssen = new Intent(SpeiseKartenActivity.this,
-					SpeiseKartenActivity.class);
-			iEssen.putExtra("name", "Essen");
-			startActivity(iEssen);
-			finish();
-			return true;
+            case R.id.speisekarte:
+                Intent iEssen = new Intent(SpeiseKartenActivity.this,
+                        SpeiseKartenActivity.class);
+                iEssen.putExtra("name", "Essen");
+                startActivity(iEssen);
+                finish();
+                return true;
 
-		case R.id.getraenkekarte:
-			Intent iGetraenke = new Intent(SpeiseKartenActivity.this,
-					SpeiseKartenActivity.class);
-			iGetraenke.putExtra("name", "Getraenke");
-			startActivity(iGetraenke);
-			finish();
-			return true;
+            case R.id.getraenkekarte:
+                Intent iGetraenke = new Intent(SpeiseKartenActivity.this,
+                        SpeiseKartenActivity.class);
+                iGetraenke.putExtra("name", "Getraenke");
+                startActivity(iGetraenke);
+                finish();
+                return true;
 
-		case R.id.action_settings:
-			finish();
-			return true;
-		}
+            case R.id.change_table:
+                finish();
+                return true;
 
-		return super.onOptionsItemSelected(item);
-	}
+            case R.id.compute_table:
+                Intent computeTableIntent = new Intent(SpeiseKartenActivity.this, AbrechnungsActivity.class);
+                //computeTableIntent.putExtra("tableNumber", "Tisch " + BedienungTischAuswahlActivity.getTNR());
+                startActivity(computeTableIntent);
+        }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		boolean result = super.onCreateOptionsMenu(menu);
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main, menu);
+        return super.onOptionsItemSelected(item);
+    }
 
-		return result;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
 
-	// RemoteDataTask AsyncTask
-	private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-			// Create a progressdialog
-			mProgressDialog = new ProgressDialog(SpeiseKartenActivity.this);
-			// Set progressdialog title
-			mProgressDialog.setTitle("Lade " + karte + "liste");
-			// Set progressdialog message
-			mProgressDialog.setMessage("Loading...");
-			mProgressDialog.setIndeterminate(false);
-			// Show progressdialog
-			mProgressDialog.show();
-		}
+        return result;
+    }
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			// Create the array
-			parselistdownloadList = new ArrayList<ParselistdownloadClass>();
-			try {
-				// Locate the class table named "Country" in Parse.com
-				ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
-						LoginSignupActivity.getParseUser() + "_" + karte);
-				query.orderByAscending("Name");
-				if (karte.length() > 6) {
-					query.orderByAscending("Kategorie");
-				} else {
-					query.orderByDescending("Kategorie");
-				}
-				ob = query.find();
-				for (ParseObject Name : ob) {
-					ParselistdownloadClass map = new ParselistdownloadClass();
-					map.setName((String) Name.get("Name"));
-					map.setPreis((Double) Name.get("Preis"));
-					map.setArt((String) Name.get("Art"));
-					map.setKategorie((String) Name.get("Kategorie"));
-					parselistdownloadList.add(map);
-				}
-			} catch (ParseException e) {
-				Log.e("Error", e.getMessage());
-				e.printStackTrace();
-			}
-			return null;
-		}
+    // RemoteDataTask AsyncTask
+    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Create a progressdialog
+            mProgressDialog = new ProgressDialog(SpeiseKartenActivity.this);
+            // Set progressdialog title
+            mProgressDialog.setTitle("Lade " + karte + "liste");
+            // Set progressdialog message
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            // Show progressdialog
+            mProgressDialog.show();
+        }
 
-		@Override
-		protected void onPostExecute(Void result) {
-			// Locate the listview in listview_main.xml
-			listview = (ListView) findViewById(R.id.list);
-			// Pass the results into ListViewAdapter.java
-			adapter = new ListViewAdapter(SpeiseKartenActivity.this,
-					parselistdownloadList);
-			// Binds the Adapter to the ListView
-			listview.setAdapter(adapter);
-			// Close the progressdialog
-			mProgressDialog.dismiss();
-		}
-	}
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Create the array
+            parselistdownloadList = new ArrayList<ParselistdownloadClass>();
+            try {
+                // Locate the class table named "Country" in Parse.com
+                ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
+                        LoginSignupActivity.getParseUser() + "_" + karte);
+                query.orderByAscending("Name");
+                if (karte.length() > 6) {
+                    query.orderByAscending("Kategorie");
+                } else {
+                    query.orderByDescending("Kategorie");
+                }
+                ob = query.find();
+                for (ParseObject Name : ob) {
+                    ParselistdownloadClass map = new ParselistdownloadClass();
+                    map.setName((String) Name.get("Name"));
+                    map.setPreis((Double) Name.get("Preis"));
+                    map.setArt((String) Name.get("Art"));
+                    map.setKategorie((String) Name.get("Kategorie"));
+                    parselistdownloadList.add(map);
+                }
+            } catch (ParseException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Locate the listview in listview_main.xml
+            listview = (ListView) findViewById(R.id.list);
+            // Pass the results into ListViewAdapter.java
+            adapter = new ListViewAdapter(SpeiseKartenActivity.this,
+                    parselistdownloadList);
+            // Binds the Adapter to the ListView
+            listview.setAdapter(adapter);
+            // Close the progressdialog
+            mProgressDialog.dismiss();
+        }
+    }
 }
--- Bierdienung/src/de/ur/mi/bierdienung/TischActivity.java
+++ Bierdienung/src/de/ur/mi/bierdienung/TischActivity.java
@@ -25,143 +25,145 @@ import de.ur.mi.login.LoginSignupActivity;
 
 public class TischActivity extends ListActivity {
 
-	public static final int INSERT_ID = Menu.FIRST;
-	public static final int TISCH_WECHSELN_ID = Menu.FIRST + 1;
-	private static final int DELETE_ID = Menu.FIRST + 1;
+    public static final int INSERT_ID = Menu.FIRST;
+    public static final int TISCH_WECHSELN_ID = Menu.FIRST + 1;
+    private static final int DELETE_ID = Menu.FIRST + 1;
 
-	private List<ParseObject> todos;
-	private ProgressDialog mProgressDialog;
+    private List<ParseObject> todos;
+    private ProgressDialog mProgressDialog;
 
-	private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
+    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
 
-		// Override this method to do custom remote calls
-		protected Void doInBackground(Void... params) {
-			// Gets the current list of todos in sorted order
-			ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
-					LoginSignupActivity.getParseUser() + "_Bestellung");
-			query.whereEqualTo("Tisch", BedienungTischAuswahlActivity.getTNR());
-			query.orderByDescending("_created_at");
+        // Override this method to do custom remote calls
+        protected Void doInBackground(Void... params) {
+            // Gets the current list of todos in sorted order
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
+                    LoginSignupActivity.getParseUser() + "_Bestellung");
+            query.whereEqualTo("Tisch", BedienungTischAuswahlActivity.getTNR());
+            query.orderByDescending("_created_at");
 
-			try {
-				todos = query.find();
-			} catch (ParseException e) {
+            try {
+                todos = query.find();
+            } catch (ParseException e) {
 
-			}
-			return null;
-		}
+            }
+            return null;
+        }
 
-		@Override
-		protected void onPreExecute() {
-			// Create a progressdialog
-			mProgressDialog = new ProgressDialog(TischActivity.this);
-			// Set progressdialog title
-			mProgressDialog.setTitle("Lade Tischliste");
-			// Set progressdialog message
-			mProgressDialog.setMessage("Loading...");
-			mProgressDialog.setIndeterminate(false);
-			// Show progressdialog
-			mProgressDialog.show();
-			super.onPreExecute();
-		}
+        @Override
+        protected void onPreExecute() {
+            // Create a progressdialog
+            mProgressDialog = new ProgressDialog(TischActivity.this);
+            // Set progressdialog title
+            mProgressDialog.setTitle("Lade Tischliste");
+            // Set progressdialog message
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            // Show progressdialog
+            mProgressDialog.show();
+            super.onPreExecute();
+        }
 
-		@Override
-		protected void onProgressUpdate(Void... values) {
+        @Override
+        protected void onProgressUpdate(Void... values) {
 
-			super.onProgressUpdate(values);
-		}
+            super.onProgressUpdate(values);
+        }
 
-		@Override
-		protected void onPostExecute(Void result) {
-			// Put the list of todos into the list view
-			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-					TischActivity.this, R.layout.activity_todo_only_textview);
-			for (ParseObject todo : todos) {
-				adapter.add((String) todo.get("Name"));
-			}
-			setListAdapter(adapter);
-			TischActivity.this.mProgressDialog.dismiss();
-		}
-	}
+        @Override
+        protected void onPostExecute(Void result) {
+            // Put the list of todos into the list view
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    TischActivity.this, R.layout.activity_todo_only_textview);
+            for (ParseObject todo : todos) {
+                adapter.add((String) todo.get("Name"));
+            }
+            setListAdapter(adapter);
+            TischActivity.this.mProgressDialog.dismiss();
+        }
+    }
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tisch);
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tisch);
 
-		setTitle("Tisch " + BedienungTischAuswahlActivity.getTNR());
+        setTitle("Tisch " + BedienungTischAuswahlActivity.getTNR());
 
-		new RemoteDataTask().execute();
-		registerForContextMenu(getListView());
-	}
+        new RemoteDataTask().execute();
+        registerForContextMenu(getListView());
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		boolean result = super.onCreateOptionsMenu(menu);
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main, menu);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
 
-		return result;
-	}
+        return result;
+    }
 
-	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,
-			ContextMenuInfo menuInfo) {
-		super.onCreateContextMenu(menu, v, menuInfo);
-		menu.add(0, DELETE_ID, 0, R.string.menu_delete);
-	}
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0, DELETE_ID, 0, R.string.menu_delete);
+    }
 
-	@Override
-	public boolean onContextItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case DELETE_ID:
-			AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
-					.getMenuInfo();
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case DELETE_ID:
+                AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+                        .getMenuInfo();
 
-			// Delete the remote object
-			final ParseObject todo = todos.get(info.position);
+                // Delete the remote object
+                final ParseObject todo = todos.get(info.position);
 
-			new RemoteDataTask() {
-				protected Void doInBackground(Void... params) {
-					try {
-						todo.delete();
-					} catch (ParseException e) {
-					}
-					super.doInBackground();
-					return null;
-				}
-			}.execute();
-			return true;
-		}
-		return super.onContextItemSelected(item);
-	}
+                new RemoteDataTask() {
+                    protected Void doInBackground(Void... params) {
+                        try {
+                            todo.delete();
+                        } catch (ParseException e) {
+                        }
+                        super.doInBackground();
+                        return null;
+                    }
+                }.execute();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
 
-		case R.id.speisekarte:
-			Intent iEssen = new Intent(TischActivity.this,
-					SpeiseKartenActivity.class);
-			iEssen.putExtra("name", "Essen");
-			startActivity(iEssen);
-			finish();
-			return true;
+            case R.id.speisekarte:
+                Intent iEssen = new Intent(TischActivity.this,
+                        SpeiseKartenActivity.class);
+                iEssen.putExtra("name", "Essen");
+                startActivity(iEssen);
+                finish();
+                return true;
 
-		case R.id.getraenkekarte:
-			Intent iGetraenke = new Intent(TischActivity.this,
-					SpeiseKartenActivity.class);
-			iGetraenke.putExtra("name", "Getraenke");
-			startActivity(iGetraenke);
-			finish();
-			return true;
+            case R.id.getraenkekarte:
+                Intent iGetraenke = new Intent(TischActivity.this,
+                        SpeiseKartenActivity.class);
+                iGetraenke.putExtra("name", "Getraenke");
+                startActivity(iGetraenke);
+                finish();
+                return true;
 
-		case R.id.action_settings:
-			finish();
-			return true;
-		}
+            case R.id.change_table:
+                finish();
+                return true;
+        }
 
-		return super.onOptionsItemSelected(item);
-	}
+        return super.onOptionsItemSelected(item);
+    }
 
 }
