cf67727815a81500bd0d3805c90d8e43d675c319
JonE <JonE@192.168.1.5>
2013-09-23 22:21:32 +0200

    so bei Tisch-Verwaltung steht jetzt drüber die aktuelle tischanzahl..

--- Bierdienung/src/de/ur/mi/bierdienung/config/TableManagementActivity.java
+++ Bierdienung/src/de/ur/mi/bierdienung/config/TableManagementActivity.java
@@ -1,28 +1,52 @@
 package de.ur.mi.bierdienung.config;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.TextView;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import de.ur.bierdienung.R;
 import de.ur.mi.login.LoginSignupActivity;
 
-// gets input of max table number in restaurant and saves that number in appSingleton
+// gets input of max table number in restaurant and saves that number
 public class TableManagementActivity extends Activity {
 	Button tableNumberFinishButton;
 	EditText tableNumberEditText;
+	TextView currentTableNumTextView;
 	private String tableNumber = "";
+	private List<ParseObject> parseListTableNumber;
+	private String maxTables = "";
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_table_management);
+		
+
+		ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
+				LoginSignupActivity.getParseUser() + "_Table");
+		try {
+			parseListTableNumber = query.find();
+
+		} catch (ParseException e) {
+			Log.e("Error", e.getMessage());
+			e.printStackTrace();
+		}
+
+		if (parseListTableNumber.size() > 0) {
+			maxTables = parseListTableNumber.get(
+					parseListTableNumber.size() - 1).getString("TableNumber");
+		}
 		setupUI();
 
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
@@ -43,6 +67,8 @@ public class TableManagementActivity extends Activity {
 
 	private void setupUI() {
 		tableNumberEditText = (EditText) findViewById(R.id.table_number_edit_text);
+		currentTableNumTextView = (TextView) findViewById(R.id.text_view_table_num);
+		currentTableNumTextView.setText("Aktuelle Tischanzahl: " + maxTables);
 		tableNumberFinishButton = (Button) findViewById(R.id.set_table_number_button);
 		tableNumberFinishButton.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -53,11 +79,11 @@ public class TableManagementActivity extends Activity {
 				ParseObject objectToSave = new ParseObject(LoginSignupActivity
 						.getParseUser() + "_Table");
 				objectToSave.put("TableNumber", tableNumber);
+
 				objectToSave.saveInBackground();
 
 				Intent preferencesIntent = new Intent(
-						TableManagementActivity.this,
-						ManagementActivity.class);
+						TableManagementActivity.this, ManagementActivity.class);
 				startActivity(preferencesIntent);
 				finish();
 			}
